512caa204846234f9d22717c203dcddbcca63fdd
Rodrigo Prestes Machado <rodrigo.prestes@gmail.com>
2015-09-26 18:54:02 -0300

    Inicial import

---
+++ src/edu/ufrgs/aeditor/websocket/AEditorWebSocket.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2015, Rodrigo Prestes Machado
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.ufrgs.aeditor.websocket;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+@ServerEndpoint("/WebSocket")
+public class AEditorWebSocket {
+
+	private static Set<Session> clients = Collections.synchronizedSet(new HashSet<Session>());
+
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		try {
+			synchronized (clients) {
+				for (Session client : clients) {
+					if (!client.getId().equals(session.getId())){
+						client.getBasicRemote().sendText(message);
+					}
+				}
+			}
+		} 
+		catch (IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	@OnOpen
+	public void onOpen(Session session) {
+		// Add session to the connected sessions set
+		clients.add(session);
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		// Remove session from the connected sessions set
+		clients.remove(session);
+	}
+
+}
